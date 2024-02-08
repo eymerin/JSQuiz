db5c2e9991b6572d4bda12698a0ccb4b36e9799a
eymerin <garrett.bryce.young@gmail.com>
2024-02-08 12:46:25 -0700

    fixed timer, added high scores display and got local storage working

--- develop/script.js
+++ develop/script.js
@@ -1,221 +1,247 @@
-//to do
-//start page                needs to have answers hidden, with centered quiz intro and start button that changes color on hover
-//questions page            on start, hide start button, show queston and answer buttons that change color on hover and start timer
-//next question             needs to update question and answer button text, display wrong or correct, and deduct time from clock if wrong
-//time runs out or finish   hide answer buttons and timer, change text in question box to show score, open form to submit initials, save score with initials
-//highscores                show top 3 scores with Initials
-
-//Declare variables
-var quizDescription = "";
+// Declare variables
+var counter = 65;
+var questionIndex = 0;
+var score = 0;
+var timerInterval;
 var scores = [];
 var highScores = [];
-var correct = "That's Right!"
-var incorrect = "Sorry, that's incorrect..."
-
-//creates buttons
+const existingScores = JSON.parse(localStorage.getItem("scores")) || [];
+const questionEl = document.getElementById("question");
+const answerBtn = document.getElementById("answer-buttons");
+const submitButton = document.getElementById("submitButton");
+const startButton = document.getElementById("startButton");
+const initialBtn = document.getElementById("initialBtn");
+const initials = document.getElementById("initialsForm");
+const userform = document.getElementById("userinitials");
 const answer1 = document.getElementById('1');
 const answer2 = document.getElementById('2');
 const answer3 = document.getElementById('3');
 const answer4 = document.getElementById('4');
-
-const questionEl = document.getElementById("question");
-const answerBtn = document.getElementById("answer-buttons");
-const submitBtn = document.getElementById("submitButton");
 const points = document.getElementById("result");
-
-
-//set starting conditions
-let questionIndex = 0;
-let score = 0;
-
-window.addEventListener('load', function() {
-
-});
-  
-// Class to add
+const welcome = "This is a quiz to test your knowledge of Javascript! Click below to get started."
 const className = "hide";
-  
-//sets quiz at first question
+
 var questions = [
     {
-      question:"What is the capital of Alaska?",
+      question:"Which character is used to call the ID on an object",
       answers:[
-        {answer:"Juneau", correct: true},
-        {answer:"Anchorage", correct: false},
-        {answer:"fish", correct: false},
-        {answer:"bears", correct: false},
+        {answer:"#", correct: true},
+        {answer:".", correct: false},
+        {answer:"!", correct: false},
+        {answer:"$", correct: false},
       ],
-      correct: 1002,
-      selected: null,
-      reason: "It's Juneau, which still has a population of less than 33,000. Yikes."
     },
     {
-      question:"Who founded the US Treasury?",
+      question:"How do you call a function?",
       answers:[
-        {answer:"Lincoln", correct: false},
-        {answer:"Washington", correct: false},
-        {answer:"jefferson", correct: false},
-        {answer:"Kenny", correct: true},
+        {answer:"$function", correct: false},
+        {answer:"function=", correct: false},
+        {answer:"function:", correct: false},
+        {answer:"function()", correct: true},
       ],
-      correct: 1004,
-      selected: null,
-      reason: "Alexander Hamilton was a major contrinutor to the strucure of the formative years of the US government. Arguably his largest contribution, apart from being a major contributor to maturing the constitution itself, was managing and growing the fledgling US credit system."
     },
     {
-      question:"Who is the first American born president?",
+      question:"Inside which HTML element do we put the JavaScript?",
       answers:[
-        {answer:"Anchorage", correct: false},
-        {answer:"Anchorage", correct: false},
-        {answer:"Anchorage", correct: true},
-        {answer:"Anchorage", correct: false},
+        {answer:"javascript", correct: false},
+        {answer:"scripting", correct: false},
+        {answer:"script", correct: true},
+        {answer:"js", correct: false},
       ],
-      correct: 1010,
-      selected: null,
-      reason: "Martin Van Buren was born in December 5th, 1782 in Kinderhook, NY. The first president not born under British rule and the first president not of British ancestry. He was of Dutch lineage."
     },     
     {
-      question:"Which was not a part of the original 13 colonies?",
+      question:'How do you write "Hello World" in an alert box?',
       answers:[
-        {answer:"Anchorage", correct: false},
-        {answer:"Anchorage", correct: true},
-        {answer:"Anchorage", correct: false},
-        {answer:"Anchorage", correct: false},
+        {answer:'msg("Hello World")', correct: false},
+        {answer:'alert("Hello World")', correct: true},
+        {answer:'msgBox("Hello World")', correct: false},
+        {answer:'alertBox("Hello World")', correct: false},
       ],
-      correct: 1011,
-      selected: null,
-      reason: "Vermont was the 14th state which joined on March 4th, 1791."
     },  
     {
-      question: "How many time zones does the USA have?",
+      question: "Which of the following type of variable takes precedence over other if names are same?",
       answers:[
-        {answer:"Anchorage", correct: true},
-        {answer:"Anchorage", correct: false},
-        {answer:"Anchorage", correct: false},
-        {answer:"Anchorage", correct: false},
+        {answer:"local variable", correct: true},
+        {answer:"global variable", correct: false},
+        {answer:"Both of the above", correct: false},
+        {answer:"None of the above", correct: false},
       ],
-      correct: 1017,
-      selected: null,
-      reason: "Eastern, Central, Mountain, Pacific, Alaskan, and Hawaii-Aleutian" 
     },    
+    {
+      question: "Which built-in method reverses the order of the elements of an array?",
+      answers:[
+        {answer:"changeOrder(order)", correct: false},
+        {answer:"reverse()", correct: true},
+        {answer:"order(reverse)", correct: false},
+        {answer:"sort(order)", correct: false},
+      ],
+    },
+    {
+      question: "Which of the following function of String object returns a number indicating the Unicode value of the character at the given index?",
+      answers:[
+        {answer:"charAt()", correct: false},
+        {answer:"concat()", correct: false},
+        {answer:"indexOf()", correct: false},
+        {answer:"charCodeAt()", correct: true},
+      ],
+    },
+    {
+      question: "Which of the following function of String object executes the search for a match between a regular expression and a specified string?",
+      answers:[
+        {answer:"concat()", correct: false},
+        {answer:"replace()", correct: false},
+        {answer:"search()", correct: true},
+        {answer:"match()", correct: false},
+      ],
+    }
 ];
 
-//Start timer
-function startTimer(){
-    var counter = 45;
-    setInterval(function() {
-      counter--;
-      if (counter >= 0) {
-        span = document.getElementById("timer");
-        span.innerHTML = counter;
-      }
-      if (counter === 0) {
-        span.innerHTML = "Time's Up!";
-        clearInterval(counter);
-      }
-    }, 1000);
-}
-
-function checkAnswer() {
-    var answer = document.getElementsByName("answers");    
-    var answer;
-    for (var i = 0; i < gender.length; i++) {
-        if (gender[i].checked === true) {
-            answer = gender[i].value;
-            break
-        }}    
-        if (answer == "Male") {        
-            alert("true")
-        } 
-        else {
-            alert("false");
-    };
-}
+window.addEventListener('load', function() {
+  questionEl.innerHTML = welcome; // Your welcome message
+  // Optionally, call showHighScores if on high scores page
+  // showHighScores();
+});
 
 function startQuiz() {
-    questionIndex = 0;
-    score = 0;
-    submitButton.innerHTML = "Submit";
-    startTimer();
-    showQuestion();
+  startButton.classList.add("hide");
+  questionIndex = 0;
+  score = 0; // Reset score
+  updateScoreDisplay(); // Update score display
+  counter = 65;
+  timer.classList.remove("hide");
+  startTimer();
+  showQuestion();
+}
+
+function startTimer() {
+  clearInterval(timerInterval);
+  timerInterval = setInterval(function() {
+    counter--;
+    if (counter > 0) {
+      document.getElementById("timer").innerHTML = `Timer: ${counter}`;
+    } else {
+      clearInterval(timerInterval);
+      document.getElementById("timer").innerHTML = "Time's Up!";
+      question.innerHTML = "Ran out of time!"
+      showScore();
+    }
+  }, 1000);
 }
 
 function showQuestion() {
-    resetState();
-    let currentQuestion = questions[questionIndex];
-    let questNum = questionIndex + 1;
-    questionEl.innerHTML = questNum + ". " + currentQuestion.question;
+  submitButton.classList.add("hide");
+  resetState(); // Call resetState to clear existing buttons before adding new ones
+  let currentQuestion = questions[questionIndex];
+  let questNum = questionIndex + 1;
+  questionEl.innerHTML = questNum + ". " + currentQuestion.question;
 
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.answer;
-        button.classList.add("answerbtn");
-        answerBtn.appendChild(button);
-        if(answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-    });
-}
-
-function showResult() {
-    span = document.getElementById("result");
-    if (answerChoice === true) {
-        span.innerHTML = correct;
-    } else {
-        span.innerHTML = incorrect;
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.answer;
+    button.classList.add("answerbtn");
+    if(answer.correct) {
+      button.dataset.correct = answer.correct;
     }
+    button.addEventListener("click", selectAnswer);
+    answerBtn.appendChild(button);
+  });
 }
 
 function resetState() {
-    while(answerBtn.firstChild) {
-        answerBtn.removeChild(answerBtn.firstChild);
-    }
+  while(answerBtn.firstChild) {
+    answerBtn.removeChild(answerBtn.firstChild);
+  }
 }
 
 function selectAnswer(e) {
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    if (isCorrect) {
-        selectedBtn.classList.add("correct");
-        score++
-    } else{
-        selectedBtn.classList.add("incorrect");
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+    if (counter > 10) {
+      counter -= 10;
+    } else {
+      counter = 0;
     }
-    Array.from(answerBtn.children).forEach(button => {
-        if (button.dataset.correct === "true") {
-            button.classList.add("correct");
-        };
-        button.disabled = true;
-    });
-    submitBtn.style.display = "block";
+  }
+  updateScoreDisplay(); // Update the score display after selecting an answer
+
+  Array.from(answerBtn.children).forEach(button => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  submitButton.classList.remove("hide");
 }
 
 function handleSubmit() {
-    questionIndex ++;
-    if(questionIndex < questions.length) {
-        showQuestion();
-    } else {
-        showScore();
-    }
+  questionIndex ++;
+  submitButton.classList.add("hide");
+  if(questionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
 }
 
 function showScore() {
-    resetState();
-    points.innerHTML = `Score: ${score}`
+  clearInterval(timerInterval);
+  // Your showScore modifications for handling score storage
+  initials.classList.remove("hide");
+  initialBtn.addEventListener('click', () => {
+    var userinitials = userform.value;
+    const newScore = { user: userinitials, score: score };
+    existingScores.push(newScore);
+    localStorage.setItem("scores", JSON.stringify(existingScores));
+    location.reload(); // Or navigate to another page
+  });
 }
 
-//starts quiz
-submitButton.addEventListener('click', () => {
-    if (submitButton.innerHTML === "Start Quiz") {
-        startQuiz();
-    } else {
-        if (questionIndex < questions.length) {
-            handleSubmit();
-        }
-    }
-    
-    submitButton.classList.add('hide');
-    // Code to start the quiz
-});
+function updateScoreDisplay() {
+  const scoreDisplay = document.getElementById("currentScore");
+  if(scoreDisplay) {
+    scoreDisplay.innerHTML = `Current Score: ${score}`;
+  }
+}
 
+function showHighScores() {
+  // Hide quiz elements
+  document.getElementById("question").classList.add("hide");
+  document.getElementById("answer-buttons").classList.add("hide");
+  document.getElementById("startButton").classList.add("hide");
+  document.getElementById("submitButton").classList.add("hide");
+  // Show the high scores container
+  document.getElementById("highScoresContainer").classList.remove("hide");
 
+  // Fetch scores from localStorage, sort them, and take the top 5
+  const scores = JSON.parse(localStorage.getItem("scores")) || [];
+  scores.sort((a, b) => b.score - a.score);
+  const topScores = scores.slice(0, 5);
+
+  // Display the top scores
+  const highScoresList = document.getElementById("highScoresList");
+  highScoresList.innerHTML = topScores.map(score => `<li>${score.user}: ${score.score}</li>`).join('');
+}
+
+// Add event listener to the High Scores link
+document.getElementById("highScores").addEventListener("click", showHighScores);
+
+function backToQuiz() {
+  // Show quiz elements
+  document.getElementById("question").classList.remove("hide");
+  document.getElementById("answer-buttons").classList.remove("hide");
+  document.getElementById("startButton").classList.remove("hide");
+  // Hide the high scores container
+  document.getElementById("highScoresContainer").classList.add("hide");
+}
+
+// Event listeners
+startButton.addEventListener('click', startQuiz);
+submitButton.addEventListener('click', handleSubmit);
+
+// Optionally call showHighScores if on the high score page
+// window.onload = showHighScores;
